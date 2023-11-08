143e630c5c656db93f2daf2d20f2b36bda058aa2
Romain <romain.avinenc@gmail.com>
2023-11-08 13:47:08 +0100

    Finish Dockerfile et docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,43 @@
-FROM node:21
+# FROM node:21
 
-# Create app directory
+# # Create app directory
+# WORKDIR /usr/src/app
+
+# # Install app dependencies
+# COPY package*.json .
+# COPY index.js .
+
+# RUN npm install
+
+# # Bundle app source
+# COPY . .
+
+# # Expose port 8080
+# EXPOSE 8080
+
+# # Run the app
+# ENTRYPOINT ["node"]
+
+# CMD ["index.js"]
+
+# ---------------------------------------------------------------------------------------------
+
+FROM node:14
+
+# Créez le répertoire de l'application
 WORKDIR /usr/src/app
 
-# Install app dependencies
+# Copiez le fichier package.json et le fichier package-lock.json s'ils existent
 COPY package*.json ./
 
+# Installez les dépendances de l'application
 RUN npm install
 
-# Bundle app source
+# Copiez tous les fichiers de l'application
 COPY . .
 
-# Expose port 8080
-EXPOSE 8080
+# Exposez le port sur lequel l'application écoute (correspond au port dans votre index.js)
+EXPOSE 3000
 
-# Run the app
-ENTRYPOINT ["node"]
-
-CMD ["index.js"]
-
-
+# Commande de démarrage de l'application
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -22,40 +22,62 @@ const Jeux = sequelize.define("Jeux", {
 
 (async () => {
   try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    await Jeux.sync();
-    await Jeux.findOrCreate({
-      where: { id: 1 },
-      defaults: {
-        nom: "Elden Ring",
-      },
-    });
-    await Jeux.findOrCreate({
-      where: { id: 2 },
-      defaults: {
-        nom: "Stellaris",
-      },
-    });
-    await Jeux.findOrCreate({
-      where: { id: 3 },
-      defaults: {
-        nom: "Superman 64",
-      },
-    });
-    console.log("Database initialized successfully.");
+    let maxAttempts = 10; // Vous pouvez ajuster ce nombre en fonction de vos besoins.
+    let delay = 5000; // 5 secondes (en millisecondes) entre chaque tentative.
+    let connected = false;
+
+    for (let attempt = 1; attempt <= maxAttempts; attempt++) {
+      try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+        connected = true;
+        break;
+      } catch (error) {
+        console.error("Unable to connect to the database:", error);
+        if (attempt < maxAttempts) {
+          console.log(`Retrying in ${delay / 1000} seconds...`);
+          await new Promise((resolve) => setTimeout(resolve, delay));
+        } else {
+          console.error("Max connection attempts reached. Exiting...");
+          process.exit(1);
+        }
+      }
+    }
+
+    if (connected) {
+      await sequelize.sync();
+      await Jeux.findOrCreate({
+        where: { id: 1 },
+        defaults: {
+          nom: "Elden Ring",
+        },
+      });
+      await Jeux.findOrCreate({
+        where: { id: 2 },
+        defaults: {
+          nom: "Stellaris",
+        },
+      });
+      await Jeux.findOrCreate({
+        where: { id: 3 },
+        defaults: {
+          nom: "Superman 64",
+        },
+      });
+      console.log("Database initialized successfully.");
+    }
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
+
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.get("/", (req, res) => {
+    Jeux.findAll().then((r) => res.send(r));
+  });
+
+  app.listen(port, () => {
+    return console.log(`Server is listening at http://localhost:${port}`);
+  });
 })();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  Jeux.findAll().then(r => res.send(r));
-});
-
-app.listen(port, () => {
-  return console.log(`Server is listening at http://localhost:${port}`);
-});
